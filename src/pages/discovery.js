cc44ac67ef8010e88ad49bf362501e36c4c6c6af
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-08-02 09:05:55 -0700

    Updated Sanity post schema and added mainImage item. Added first piece of content to projects page.

---
+++ src/pages/discovery.js
@@ -0,0 +1,18 @@
+import React from "react"
+import styled from "styled-components"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import { Container } from "../components/layout"
+
+const DiscoveryPage = () => (
+    <Layout>
+        <SEO title="Discovery" />
+        <Container>
+            
+        </Container>
+    </Layout>
+)
+
+export default DiscoveryPage
